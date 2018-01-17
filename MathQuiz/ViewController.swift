e1b33ecb799cacc2c66c44c7791a00867c9a1991
Student <student@cvs-r213d011.nic.bc.ca>
2018-01-17 11:07:46 -0800

    Second Commit

--- MathQuiz/ViewController.swift
+++ MathQuiz/ViewController.swift
@@ -10,8 +10,99 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number1: UILabel!
+    @IBOutlet weak var sign: UILabel!
+    @IBOutlet weak var number2: UILabel!
+    @IBOutlet weak var answerProvided: UITextField!
+    @IBOutlet weak var level: UILabel!
+    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var round: UILabel!
+    var randNum = 0
+    var randNum1 = 0
+    var randNum2 = 0
+
+    var realAnswer = 0
+
+
+
+    func setNum() {
+        randNum = Int(arc4random_uniform(3))
+        randNum1 = Int(arc4random_uniform(10))
+        randNum2 = Int(arc4random_uniform(10))
+
+        switch randNum {
+        case 0:
+            sign.text = "+"
+            break
+        case 1:
+            sign.text = "-"
+            break
+        case 2:
+            sign.text = "*"
+            break
+        case 3:
+            sign.text = "/"
+            break
+        default:
+            break
+        }
+        number1.text = String(randNum1)
+        number2.text = String(randNum2)
+    }
+
+
+    func findRealAnswer() {
+        if (randNum == 0) {
+            realAnswer = Int(number1.text!)! + Int(number2.text!)!
+        }
+            else if (randNum == 1) {
+                realAnswer = Int(number1.text!)! - Int(number2.text!)!
+        }
+            else if (randNum == 2) {
+                realAnswer = Int(number1.text!)! * Int(number2.text!)!
+        }
+            else {
+                realAnswer = Int(number1.text!)! / Int(number2.text!)!
+        }
+
+    }
+
+    func check() {
+        if (Int(realAnswer) == Int(answerProvided.text!)) {
+            level.text = String(Int(score.text!)! / 3)
+            score.text = String(Int(score.text!)! + 1)
+            round.text = String(Int(round.text!)! + 1)
+        } else {
+            round.text = String(Int(round.text!)! + 1)
+        }
+
+    }
+
+
+
+
+    func displayMsg() {
+        if (answerProvided.text == "") {
+            let emptyAlert = UIAlertController(title: "Error", message: "Please enter a number.", preferredStyle: .alert)
+            let emptyAction = UIAlertAction(title: "Okay", style: .default)
+            emptyAlert.addAction(emptyAction)
+            present(emptyAlert, animated: true, completion: nil)
+        }
+
+        let alert = UIAlertController(title: "You Got it!", message: "You Won \(score.text ?? "") times in this Level", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Okay", style: .default)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+
+
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNum()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +111,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func checkBtn(_ sender: Any) {
+        findRealAnswer()
+        check()
+        setNum()
+        displayMsg()
+    }
+
 
 }
-
